dafb16edb93665ff452748f022651ce0fe80f751
princekumar404 <princekumar354756@gmail.com>
2025-12-04 15:48:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
        this is a kandi brother app
+    </p>
         <a
           className="App-link"
           href="https://reactjs.org"
