8a41e32ffeb6a32fc9c412e1795603613dc3ca2e
princekumar404 <princekumar354756@gmail.com>
2025-12-04 14:51:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is a demo webapps prince
+          this is a demo webapps prince website
         </p>
         <a
           className="App-link"
