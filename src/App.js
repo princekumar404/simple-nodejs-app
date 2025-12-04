b6a774ec6b6eaabb664d254d82d4a2f8a7dcd04e
princekumar404 <princekumar354756@gmail.com>
2025-12-04 16:25:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       this is a kandi brother app
+      this is my app
     </p>
         <a
           className="App-link"
