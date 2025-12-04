61ef226cafaf5b57deaac1d46808a7a46a171ce2
princekumar404 <princekumar354756@gmail.com>
2025-12-04 15:45:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       this is a node js apps
-        </p>
+       this is a kandi brother app
         <a
           className="App-link"
           href="https://reactjs.org"
