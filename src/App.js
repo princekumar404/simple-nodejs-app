775c92a8f9da925b77359ed4365c68cfe4c72151
princekumar404 <princekumar354756@gmail.com>
2025-12-04 15:38:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       this is a node js app
+       this is a node js apps
         </p>
         <a
           className="App-link"
